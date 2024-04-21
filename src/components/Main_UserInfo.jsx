9b2d155b0f7d70082e55c0d797dd8335dcc3c594
holololob <jungkyeom@gmail.com>
2024-04-21 10:52:18 +0900

    Modify: 길이 단위 변경

--- src/components/Main_UserInfo.jsx
+++ src/components/Main_UserInfo.jsx
@@ -4,74 +4,6 @@ import Main_UserProfileImage from './Main_UserProfileImage';
 import Main_UserInfoInput from './Main_UserInfoInput';
 import Button from './Button';
 
-const UserInfo = styled.div`
-  width: 570px;
-  border: 1px solid #C8CCE5;
-  border-radius: 10px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-family: 'Pretendard';
-  background-color: white;
-  z-index: 2;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const Header = styled.div`
-  width: 100%;
-  height: 38px;
-  margin-top: 13px;
-  margin-left: 15px;
-  font-size: 30px;
-  font-weight: bold;
-`;
-
-const SectionH2 = styled.h2`
-  width: 480px;
-  display: block;
-  margin-bottom: 20px;  
-  font-size: 20px;
-  font-weight: bold;
-`;
-
-const SectionBox1 = styled.div`
-  width: 520px;
-  height: 80px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 1px solid #C8CCE5;
-  box-sizing: border-box;
-`;
-
-const SectionBox2 = styled.div`
-  width: 520px;
-  height: 190px;
-  margin: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 1px solid #C8CCE5;
-  box-sizing: border-box;
-`;
-
-const SectionBox3 = styled.div`
-  width: 520px;
-  height: 190px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-`;
-
 function Main_UserInfo() {
   return (
     <UserInfo>
@@ -97,3 +29,79 @@ function Main_UserInfo() {
 };
 
 export default Main_UserInfo; 
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 2;
+
+  width: 570px;
+  border: 1px solid #C8CCE5;
+  border-radius: 10px;
+  box-sizing: border-box;
+  
+  background-color: white;
+
+  transform: translate(-50%, -50%);
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 38px;
+  margin-top: 13px;
+  margin-left: 15px;
+
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const SectionH2 = styled.h2`
+  display: block;
+  
+  width: 480px;
+  margin-bottom: 20px;  
+
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const SectionBox1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  
+  width: 520px;
+  height: 80px;
+  border-bottom: 1px solid #C8CCE5;
+  box-sizing: border-box;
+`;
+
+const SectionBox2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  width: 520px;
+  height: 190px;
+  margin: 10px;
+  border-bottom: 1px solid #C8CCE5;
+  box-sizing: border-box;
+`;
+
+const SectionBox3 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 520px;
+  height: 190px;
+  box-sizing: border-box;
+`;
--- src/components/Main_UserProfile.jsx
+++ src/components/Main_UserProfile.jsx
@@ -5,67 +5,6 @@ import Main_UserInfo from './Main_UserInfo';
 import Main_TimeTable from './Main_TimeTable.jsx';
 import settingImage from '../assets/settingIcon.png/';
 
-const Section = styled.div`
-  width: 700px;
-  height: 445px;
-  background-color: #FFFFFF;
-  border: 2px solid #C8CCE5;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Header = styled.div`
-  width: 659px;
-  height: 38px;
-  margin-top: 13px;
-  margin-left: 15px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const HeaderH1 = styled.h1`
-  font-size: 30px;
-  font-weight: 700;
-`;
-
-const HeaderH2 = styled.h2`
-  font-size: 26px;
-  font-weight: 700;
-`;
-
-const HeaderH3 = styled.h3`
-  font-size: 20px;
-  color: gray;
-`;
-
-const HeaderSetting = styled.img`
-  width: 25px;
-  height: 25px;
-  cursor: pointer;
-`;
-
-const SectionBox = styled.div`
-  width: 180px;
-  height: 70px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Backdrop = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 1; /* 모달(2)보다 아래에 위치 */
-  display: ${({ $showModal }) => ($showModal ? 'block' : 'none')}; /* showModal 상태에 따라 표시 여부 설정 */
-`;
-
 function Main_UserProfile() {
   const [showModal, setShowModal] = useState(false); // 모달 열림 여부 상태 관리
 
@@ -79,22 +18,94 @@ function Main_UserProfile() {
   };
 
 return (
-  <Section>
-    <Header>
-      <HeaderH1>유저 프로필</HeaderH1>
-      <HeaderSetting src={settingImage} onClick={openModal}></HeaderSetting>
-    </Header>
+  <ProfileSection>
+
+    <ProfileHeader>
+      <ProfileH1>유저 프로필</ProfileH1>
+      <SettingImg src={settingImage} onClick={openModal}></SettingImg>
+    </ProfileHeader>
 
     <Main_UserProfileImage></Main_UserProfileImage>
-    <SectionBox>
-      <HeaderH2>신콩이</HeaderH2>
-      <HeaderH3>Frontend</HeaderH3>
-    </SectionBox>
+
+    <ProfileContentDiv>
+      <ProfileH2>신콩이</ProfileH2>
+      <ProfileH3>Frontend</ProfileH3>
+    </ProfileContentDiv>
+
     <Main_TimeTable></Main_TimeTable>
+
     <Backdrop $showModal={showModal} onClick={closeModal} /> {/* 모달이 열릴 때만 배경 레이어 표시 */}
       {showModal && <Main_UserInfo closeModal={closeModal} />}
-  </Section>
+  </ProfileSection>
 );
 }
 
 export default Main_UserProfile;
+
+const ProfileSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  min-width: 600px;
+  min-height: 458px;
+  border: 2px solid #C8CCE5;
+  border-radius: 10px;
+
+  background-color: #FFFFFF;
+`;
+
+const ProfileHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  
+  width: 100%;
+  margin: 20px;
+`;
+
+const ProfileH1 = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+`;
+
+const ProfileH2 = styled.h2`
+  font-size: 26px;
+  font-weight: 700;
+`;
+
+const ProfileH3 = styled.h3`
+  font-size: 20px;
+  color: gray;
+`;
+
+const SettingImg = styled.img`
+  width: 25px;
+  height: 25px;
+  padding: 2.5px;
+
+  cursor: pointer;
+`;
+
+const ProfileContentDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 180px;
+  height: 70px;
+`;
+
+const Backdrop = styled.div`
+  display: ${({ $showModal }) => ($showModal ? 'block' : 'none')}; /* showModal 상태에 따라 표시 여부 설정 */
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1; /* 모달(2)보다 아래에 위치 */
+
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.5);
+`;
